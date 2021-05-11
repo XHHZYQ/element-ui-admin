c5f29be6c7af2be0cd9e229a11246f2d0961c6dd
徐宏辉 <31964432+XHHZYQ@users.noreply.github.com>
2021-05-11 16:52:43 +0800

    修改npm包名为element-ui-admin-x

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import hasPermi from './utils/directive/hasPermi';
 
 import Form from './package'; // 直接引入文件
 // import Form from '../dist'; // 打包结果引入 dist
-// import xhhPkg from 'element-ui-admin'; // npm包引入
+// import xhhPkg from 'element-ui-admin-x'; // npm包引入
 
 import App from './example/form-demo.vue';
 // import App from './example/table-demo.vue';
